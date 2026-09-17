d71c6c0b9390749cf969bd7ab13baad700a01471
youssef elghonaimy <elghonaimyyoussef@gmail.com>
2026-09-17 09:24:05 +0300

    config, bas&data controllers, base &data routes

---
+++ src/controllers/DataController.py
@@ -0,0 +1,16 @@
+from .BaseController import BaseController
+from fastapi import UploadFile
+
+class DataController(BaseController):
+
+    def __init__(self):
+        super().__init__()
+
+    def validate_uploaded_file(self, file: UploadFile):
+        return False
+
+    if file.size > self.app_settings.FILE_MAX_SIZE * self.size_scale:
+        return False
+    if file.content_type not in self.app_settings.FILE_ALLOWED_TYPES:
+        return False
+    return True
--- src/main.py
+++ src/main.py
@@ -1,10 +1,7 @@
 from fastapi import FastAPI
-from dotenv import load_dotenv
 
-load_dotenv(".env")
-
-from routes import base
-
+from routes import base, Data
 app = FastAPI()
 
+app.include_router(Data.data_router)
 app.include_router(base.base_router)
